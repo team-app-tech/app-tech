9d0c554e673d207eae9fb6d24f6a0606507d84f4
yeongsang2 <dudtkd1221@gmail.com>
2024-02-07 19:28:37 +0900

    init project

---
+++ src/main/java/server/apptech/AppTechApplication.java
@@ -0,0 +1,13 @@
+package server.apptech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/server/apptech/AppTechApplicationTests.java
@@ -0,0 +1,13 @@
+package server.apptech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
