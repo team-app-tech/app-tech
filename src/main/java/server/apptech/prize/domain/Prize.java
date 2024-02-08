1487d6e14f8826844c1689449143ad5780b039cc
yeongsang2 <dudtkd1221@gmail.com>
2024-02-08 14:49:29 +0900

    feat : add Prize entity

---
+++ src/main/java/server/apptech/prize/domain/Prize.java
@@ -0,0 +1,35 @@
+package server.apptech.prize.domain;
+
+import jakarta.persistence.*;
+import server.apptech.comment.comment.Comment;
+import server.apptech.event.domain.Event;
+import server.apptech.global.domain.BaseEntity;
+import server.apptech.user.domain.User;
+
+import java.awt.*;
+
+@Entity
+public class Prize extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "prize_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name= "user_id")
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="comment_id")
+    private Comment comment;
+
+    private Long price;
+
+    private Integer ranking;
+
+}
