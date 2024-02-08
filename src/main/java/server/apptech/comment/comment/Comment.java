3f596e04d98954c8e95b0cdd422ac7ff02855c40
yeongsang2 <dudtkd1221@gmail.com>
2024-02-08 14:48:27 +0900

    feat : add Comment entity

---
+++ src/main/java/server/apptech/comment/comment/Comment.java
@@ -0,0 +1,30 @@
+package server.apptech.comment.comment;
+
+
+import jakarta.persistence.*;
+import server.apptech.advertisement.domain.Advertisement;
+import server.apptech.global.domain.BaseEntity;
+import server.apptech.user.domain.User;
+
+@Entity
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="advertisement_id")
+    private Advertisement advertisement;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="parent_id")
+    private Comment parent;
+
+    private String content;
+}
