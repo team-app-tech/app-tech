e62768d6ea02f75b2ab95828f195311e82060ddf
yeongsang2 <dudtkd1221@gmail.com>
2024-02-13 15:10:00 +0900

    feat : File entity 수정  - builder 생성자 패턴  - 연관관계 편의 메소드 생성

--- src/main/java/server/apptech/file/domain/File.java
+++ src/main/java/server/apptech/file/domain/File.java
@@ -1,11 +1,17 @@
 package server.apptech.file.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import server.apptech.advertisement.domain.Advertisement;
 import server.apptech.comment.comment.Comment;
 import server.apptech.global.domain.BaseEntity;
 
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class File extends BaseEntity {
 
     @Id
@@ -22,10 +28,19 @@ public class File extends BaseEntity {
     private Comment comment;
 
     @Column(name = "file_type")
+    @Enumerated(EnumType.STRING)
     private FileType fileType;
     @Column(name = "uuid")
     private String uuid;
 
     @Column(name = "url")
     private String url;
+
+    public void belongToAdvertisement(Advertisement advertisement){
+        this.advertisement = advertisement;
+    }
+
+    public void assignFileType(FileType fileType){
+        this.fileType = fileType;
+    }
 }
