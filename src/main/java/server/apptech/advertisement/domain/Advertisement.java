73bb9c3cc83539c2daefe5270fc692eda512bd9b
yeongsang2 <dudtkd1221@gmail.com>
2024-02-13 15:09:03 +0900

    feat : AdvertisementService entity 변경  builder 생성자 패턴,  event 의존관계 추가

--- src/main/java/server/apptech/advertisement/domain/Advertisement.java
+++ src/main/java/server/apptech/advertisement/domain/Advertisement.java
@@ -1,11 +1,25 @@
 package server.apptech.advertisement.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import server.apptech.advertisement.AdCreateRequest;
 import server.apptech.event.domain.Event;
+import server.apptech.file.domain.File;
 import server.apptech.global.domain.BaseEntity;
 import server.apptech.user.domain.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Advertisement extends BaseEntity {
 
     @Id
@@ -17,6 +31,10 @@ public class Advertisement extends BaseEntity {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
     @Column(name = "title")
     private String title;
 
@@ -25,4 +43,21 @@ public class Advertisement extends BaseEntity {
     @Column(name = "view_cnt")
     private Integer viewCnt;
 
+    @OneToMany(mappedBy = "advertisement")
+    private List<File> files = new ArrayList<>();
+
+    public static Advertisement of(AdCreateRequest adCreateRequest, Event event, User user){
+        return Advertisement.builder()
+                .title(adCreateRequest.getTitle())
+                .user(user)
+                .event(event)
+                .files(new ArrayList<>())
+                .content(adCreateRequest.getContent())
+                .build();
+    }
+
+    public void addFile(File file){
+        files.add(file);
+        file.belongToAdvertisement(this);
+    }
 }
