13915b521b1f3e779423d819a82b0cf00b1d7dc2
yeongsang2 <dudtkd1221@gmail.com>
2024-02-14 13:44:09 +0900

    feat : 광고 조회 기능 추가

--- src/main/java/server/apptech/advertisement/AdvertisementController.java
+++ src/main/java/server/apptech/advertisement/AdvertisementController.java
@@ -2,11 +2,11 @@ package server.apptech.advertisement;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import server.apptech.event.domain.EventStatus;
 
 import java.io.IOException;
 import java.util.List;
@@ -24,5 +24,16 @@ public class AdvertisementController {
                 .body(advertisementId);
     }
 
+    @GetMapping("/api/advertisement")
+    public ResponseEntity<?> getAdvertisements(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "5") int size,
+            @RequestParam(value = "eventStatus", required = false, defaultValue = "ONGOING") EventStatus eventStatus,
+            @RequestParam(value = "sortOption", required = false, defaultValue = "PRIZE_DESCENDING") SortOption sortOption) {
+
+        Page<AdResponse> adResponses =  advertisementService.getAdvertisements(page,size, eventStatus, sortOption);
+        return ResponseEntity.ok()
+                .body(adResponses);
+    }
 
 }
--- src/main/java/server/apptech/advertisement/AdvertisementService.java
+++ src/main/java/server/apptech/advertisement/AdvertisementService.java
@@ -2,16 +2,19 @@ package server.apptech.advertisement;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import server.apptech.advertisement.domain.Advertisement;
-import server.apptech.event.domain.Event;
+import server.apptech.event.domain.EventStatus;
 import server.apptech.file.FIleUploadService;
 import server.apptech.user.UserRepository;
 import server.apptech.user.domain.User;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -32,9 +35,7 @@ public class AdvertisementService {
         User tempUser = User.createTempuser();
         userRepository.save(tempUser);
 
-        Event event = Event.of(adCreateRequest);
-
-        Advertisement advertisement = Advertisement.of(adCreateRequest, event, tempUser);
+        Advertisement advertisement = Advertisement.of(adCreateRequest, tempUser);
 
         if(multipartFiles != null){
             for (MultipartFile multipartFile : multipartFiles){
@@ -42,5 +43,96 @@ public class AdvertisementService {
             }
         }
         return advertisementRepository.save(advertisement).getId();
+    }
+
+    public Page<AdResponse> getAdvertisements(int page, int size, EventStatus eventStatus, SortOption sortOption) {
+
+        PageRequest pageable = PageRequest.of(page, size);
+
+        Page<Advertisement> advertisements = null;
+
+        switch (sortOption){
+            case PRIZE_DESCENDING:
+                if(eventStatus == EventStatus.UPCOMING){
+                    advertisements =advertisementRepository.findByUpComingOrderByPrizeDesc(pageable,LocalDateTime.now());
+
+                }
+                if(eventStatus == EventStatus.ONGOING){
+                    advertisements =advertisementRepository.findByOnGoingOrderByPrizeDesc(pageable,LocalDateTime.now());
+
+                }
+                if(eventStatus == EventStatus.FINISHED){
+                    advertisements = advertisementRepository.findByFinishedOrderByPrizeDesc(pageable,LocalDateTime.now());
+                }
+                break;
+            case START_ASCENDING:
+                if(eventStatus == EventStatus.UPCOMING){
+                    advertisements =advertisementRepository.findByUpComingOrderByStartDateAsc(pageable,LocalDateTime.now());
+
+                }
+                if(eventStatus == EventStatus.ONGOING){
+                    advertisements =advertisementRepository.findByOnGoingOrderByStartDateAsc(pageable,LocalDateTime.now());
+
+                }
+                if(eventStatus == EventStatus.FINISHED){
+                    advertisements =advertisementRepository.findByFinishedOrderByStartDateAsc(pageable,LocalDateTime.now());
+                }
+                break;
+            case END_ASCENDING:
+                if(eventStatus == EventStatus.UPCOMING){
+                    advertisements =advertisementRepository.findByUpComingOrderByEndDateAsc(pageable,LocalDateTime.now());
+
+                }
+                if(eventStatus == EventStatus.ONGOING){
+                    advertisements =advertisementRepository.findByOnGoingOrderByEndDateAsc(pageable,LocalDateTime.now());
+
+                }
+                if(eventStatus == EventStatus.FINISHED){
+                    advertisements =advertisementRepository.findByFinishedOrderByEndDateAsc(pageable,LocalDateTime.now());
+                }
+                break;
+            case VIEWS_DESCENDING:
+                if(eventStatus == EventStatus.UPCOMING){
+                    advertisements =  advertisementRepository.findByUpComingOrderByViewCnt(pageable, LocalDateTime.now());
+
+                }
+                if(eventStatus == EventStatus.ONGOING){
+                    advertisements =  advertisementRepository.findByOngoingOrderByViewCnt(pageable, LocalDateTime.now());
+
+                }
+                if(eventStatus == EventStatus.FINISHED){
+                    advertisements =  advertisementRepository.findByFinishedOrderByViewCnt(pageable, LocalDateTime.now());
+                }
+                break;
+            //case COMMENTS_DESCENDING:
+//                if(eventStatus == EventStatus.UPCOMING){
+//                    advertisements =  advertisementRepository.findByUpComingOrderByCommentCnt(pageable, LocalDateTime.now());
+//
+//                }
+//                if(eventStatus == EventStatus.ONGOING){
+//                    advertisements =  advertisementRepository.findByOngoingOrderByCommentCnt(pageable, LocalDateTime.now());
+//
+//                }
+//                if(eventStatus == EventStatus.FINISHED){
+//                    advertisements =  advertisementRepository.findByFinishedOrderByCommentsCnt(pageable, LocalDateTime.now());
+//                }
+//                break;
+//            case LIKES_DESCENDING:
+//                if(eventStatus == EventStatus.UPCOMING){
+//                    advertisements =  advertisementRepository.findByUpComingOrderByLikeCnt(pageable, LocalDateTime.now());
+//
+//                }
+//                if(eventStatus == EventStatus.ONGOING){
+//                    advertisements =  advertisementRepository.findByOngoingOrderByLikeCnt(pageable, LocalDateTime.now());
+//
+//                }
+//                if(eventStatus == EventStatus.FINISHED){
+//                    advertisements =  advertisementRepository.findByFinishedOrderByLikeCnt(pageable, LocalDateTime.now());
+//                }
+//                break;
+            default:
+                throw new RuntimeException("존재하지 않는 타입");
+        }
+        return advertisements.map(advertisement -> AdResponse.of(advertisement));
     }
 }
