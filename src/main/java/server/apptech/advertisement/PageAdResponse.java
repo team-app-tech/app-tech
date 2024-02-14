e7c536bececffa582a833158c7cf0200f36a52a9
yeongsang2 <dudtkd1221@gmail.com>
2024-02-14 14:17:01 +0900

    fix : 광고 목록 조회시 반환타입 dto생성  Page<AdResponse> -> PageAdResponse로 변환

---
+++ src/main/java/server/apptech/advertisement/PageAdResponse.java
@@ -0,0 +1,26 @@
+package server.apptech.advertisement;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class PageAdResponse {
+
+    private List<AdResponse> content;
+    private int totalPage;
+    private int currentPage;
+    private long totalElements;
+
+    public static PageAdResponse of(Page<AdResponse> adResponses){
+        return PageAdResponse.builder()
+                .content(adResponses.getContent())
+                .totalPage(adResponses.getTotalPages())
+                .currentPage(adResponses.getPageable().getPageNumber())
+                .totalElements(adResponses.getTotalElements())
+                .build();
+    }
+}
