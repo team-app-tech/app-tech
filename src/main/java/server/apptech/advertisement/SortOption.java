0a404465162d842d29e1bd5e5e4e3bebe1054489
yeongsang2 <dudtkd1221@gmail.com>
2024-02-14 13:43:42 +0900

    feat : sortOption 추가  - 정렬 기준을 추가합니다.

--- src/main/java/server/apptech/advertisement/SortOption.java
+++ src/main/java/server/apptech/advertisement/SortOption.java
@@ -4,7 +4,7 @@ public enum SortOption {
 
     PRIZE_DESCENDING,   // 상금순
     START_ASCENDING,    // 시작순
-    END_ASDCENDING, // 마감순
+    END_ASCENDING, // 마감순
     VIEWS_DESCENDING,   // 조회순
     COMMENTS_DESCENDING,// 댓글순
     LIKES_DESCENDING  // 하트순
