2bd8b8d85f7ae1832c498ad4940ce886bf68c593
yeongsang2 <dudtkd1221@gmail.com>
2024-02-15 21:48:37 +0900

    test : AdvertisementServiceTest 작성

--- src/test/java/server/apptech/AppTechApplicationTests.java
+++ src/test/java/server/apptech/AppTechApplicationTests.java
@@ -2,12 +2,14 @@ package server.apptech;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 class AppTechApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
