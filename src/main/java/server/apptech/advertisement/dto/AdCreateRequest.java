43aad540089d2786807f6e6c37aced4f75183c07
yeongsang2 <dudtkd1221@gmail.com>
2024-02-26 10:29:24 +0900

    fix : AdCreateRequest 어노테이션 추가  - @AllArgsConstructor  - @NoArgsConstructor(access = AccessLevel.PROTECTED) 추가

--- src/main/java/server/apptech/advertisement/dto/AdCreateRequest.java
+++ src/main/java/server/apptech/advertisement/dto/AdCreateRequest.java
@@ -1,13 +1,14 @@
 package server.apptech.advertisement.dto;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AdCreateRequest {
 
     private String title;
