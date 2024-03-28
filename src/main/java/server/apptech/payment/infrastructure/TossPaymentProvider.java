894f1926c8e5037035a75e97485daf8ea7b21e78
yeongsang2 <dudtkd1221@gmail.com>
2024-03-28 13:20:56 +0900

    fix : 예외 처리 로직 수정

--- src/main/java/server/apptech/payment/infrastructure/TossPaymentProvider.java
+++ src/main/java/server/apptech/payment/infrastructure/TossPaymentProvider.java
@@ -5,8 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import server.apptech.global.exception.ExceptionCode;
 import server.apptech.global.exception.InvalidPaymentException;
 import server.apptech.payment.dto.PaymentRequest;
@@ -15,6 +15,7 @@ import server.apptech.payment.domain.PaymentProvider;
 import static java.util.Base64.*;
 
 @Component
+@Slf4j
 public class TossPaymentProvider implements PaymentProvider {
 
     private final String baseUri;
@@ -29,16 +30,16 @@ public class TossPaymentProvider implements PaymentProvider {
     public PaymentInfo confirmPayment(PaymentRequest paymentRequest) {
 
         String encodedCredentials = getEncoder().encodeToString(secretKey.getBytes());
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.set("Authorization", "Basic " + encodedCredentials);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Authorization", "Basic " + encodedCredentials);
-        HttpEntity<PaymentRequest> requestEntity = new HttpEntity<>(paymentRequest, headers);
-        ResponseEntity<PaymentInfo> responseEntity = restTemplate.postForEntity(baseUri + CONFIRM, requestEntity, PaymentInfo.class);
-
-        if(responseEntity.getStatusCode().is2xxSuccessful()){
-            return responseEntity.getBody();
+            HttpEntity<PaymentRequest> requestEntity = new HttpEntity<>(paymentRequest, headers);
+            PaymentInfo paymentInfo = restTemplate.postForObject(baseUri + CONFIRM, requestEntity, PaymentInfo.class);
+            return paymentInfo;
+        }catch (HttpClientErrorException ex){
+            throw new InvalidPaymentException(ExceptionCode.INVALID_PAYMENT_REQUEST);
         }
-        throw new InvalidPaymentException(ExceptionCode.INVALID_PAYMENT_REQUEST);
     }
 }
